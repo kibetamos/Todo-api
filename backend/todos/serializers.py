e6587eda21f32b1d2d942c57de1d435ae560ac50
kibet kilel <amos.rotich@strathmore.edu>
2022-04-11 10:58:17 +0300

    Update

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers # we import the serializer fom the rest f
 from .models import Todo #import models from todoapp
 
 #create a serializer classs
-class TodoSerializer(serializers.modelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     # We’re specifying which model to use and the specific fields on it we want to expose
     class Meta:
-        model=todo
+        model = Todo
         fields = ('id', 'title', 'body',)
 # Django REST Framework will now magically transform our data into JSON exposing
 # the fields for id, title, and body from our Todomodel.
+#we serailze so that we get our data in form of JSON format
+
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+# we import Django REST Framework’s generics views and both our models.py and
+# serializers.py files.
 
 # Create your views here.
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+# from our todos/urls.py we had two views.
+# so in our model We will use ListAPIView37 to
+# display all todos and RetrieveAPIView38 to display a single model
+# instance
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
